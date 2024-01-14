67257fe1eee1a831b5dc923b7b585ba32e58bab6
name <name@example.com>
2024-01-15 01:47:08 +0300

    in progress

--- day_12_part_2/main.py
+++ day_12_part_2/main.py
@@ -1,6 +1,13 @@
 from typing import List, Iterator
 import re
 
+class Record:
+    def __init__(self, value: str) -> None:
+        self.value = value
+        self.bad = int(value.replace('.', '0').replace('?', '0').replace('#', '1'), 2)
+        self.good = int(value.replace('.', '1').replace('?', '0').replace('#', '0'), 2)
+        self.unknown = int(value.replace('.', '0').replace('?', '1').replace('#', '0'), 2)
+
 def parse_input(filename: str) -> List[tuple[str,List[int]]]:
     f = open(filename, 'r', encoding='utf-8')
     output = []
@@ -10,36 +17,12 @@ def parse_input(filename: str) -> List[tuple[str,List[int]]]:
         repdata = repdata.removesuffix('\n')
         #repdata = repdata + (',' + repdata) * 4
         repdata = [int(x) for x in repdata.split(',')]
-        output.append((springs, repdata))
+        output.append((Record(springs), repdata))
     return output
 
-def find_gaps(springs: str, search_start = 0) -> List[tuple[int,int]]:
-    #takes a string for springs condition and returns a list of (pos, length) for every contigious chunk of [#?]
-    gaps = []
-    start = next((x for x in range(search_start, len(springs)) if springs[x] == '#' or springs[x] == '?'), None)
-    if start is not None:
-        pos = springs.find('.', start)
-        end = len(springs) if pos == -1 else pos
-        gaps.append((start, end - start))
-        gaps.extend(find_gaps(springs, end))
-    return gaps
+
+def count_placements(rec: Record, chunks: List[int]) -> List[str]:
     
 
-def get_first_chunk_placements(conditions: str, chunks: List[int]) -> int:
-    gaps = []
-    flag = False
-    length = 0
-
-
-    right_bound = min(\
-            len(conditions) - (sum(chunks[1:]) + len(chunks[1:])),\
-            len(conditions) if conditions.find('#') == -1 else (conditions.find('#') + chunks[0])\
-        )
-    re_string = '(?=([#?]{'+str(chunks[0])+'}))'
-    matches = re.finditer(re_string, conditions[:right_bound])
-    return [(x[1], x.pos) for x in matches]
 
 input = parse_input('input.txt')
-a,b = input[2]
-print(a)
-print(find_gaps(a))
