823daef30f813974bf199a16218d6770b62a53aa
name <name@example.com>
2024-04-29 01:13:19 +0300

    painfully slow but maybe works?

--- day_17_day_1/main.py
+++ day_17_day_1/main.py
@@ -2,7 +2,8 @@ from __future__ import annotations
 from typing import List, NamedTuple, Iterator
 from enum import Enum
 from collections import deque, defaultdict
-from itertools import groupby
+from functools import cache
+
 
 class Coords(NamedTuple):
     x: int
@@ -19,41 +20,6 @@ class Edge(NamedTuple):
     destination: Coords
     distance: int
     direction: Direction
-
-class Node:
-    def __init__(self, data: Coords) -> None:
-        super().__init__()
-        self.data: Coords = data
-        self.children: set[Node] = set()
-        self.parent: Node = None
-
-class Tree:
-    def __init__(self, data: Coords) -> None:
-        super().__init__()
-        node = Node(data)
-        self.root = node
-        self.leafs = {node}
-        self.length = 1
-
-    def append(self, child: Tree) -> None:
-        #we only append to the root and all trees we append are the same length
-        self.root.children.add(child.root)
-        #if there's only root in the tree - remove it from list of leaves
-        if self.length == 1:
-            self.leafs.remove(self.root)
-        child.root.parent = self.root
-        self.leafs.update(child.leafs)
-        self.length = child.length + 1
-
-    def trim(self) -> None:
-        #remove last nodes in the tree
-        new_leafs = {leaf.parent for leaf in self.leafs}
-        for leaf in new_leafs:
-            leaf.children = {}
-        self.leafs = new_leafs
-        self.length -= 1
-
-        
 
 def parse_input(filename: str) -> List[List[int]]:
     f = open(filename, mode='r', encoding='utf-8')
@@ -89,7 +55,7 @@ def find_shortest_distance(nodes: List[Coords], edges: List[Edge], start: Coords
     #calc min distances first, then restore the path of minimal distance (or multiple paths if there are more than one)
     #for a path of every length(in number of edges it consists of) we calculate minimal distance we can reach every node and last edge in the path (or multiple edges)
     #obviously not every node is reachable in arbitrary amount of steps
-    max_straight = 3
+    max_straight = 4
 
     node_inputs: dict[Coords, List[Edge]] = defaultdict(list)
     for edge in edges:
@@ -98,39 +64,54 @@ def find_shortest_distance(nodes: List[Coords], edges: List[Edge], start: Coords
     INF = float('inf')
     previous: dict[Coords, int|float] = defaultdict(lambda: INF)
     shortest: dict[Coords, int|float] = defaultdict(lambda: INF)
-    backlog: dict[Coords, Tree[Coords]] = {}
+    backlog: deque[dict[Coords, List[Coords]]] = deque([], maxlen=max_straight)
     previous[start] = 0
-    backlog[start] = Tree(start)
 
-    def is_valid(edge: Edge, backlogdict: dict[Coords, Tree[Coords]], max_straight = max_straight) -> bool:
-        backlog = backlogdict[edge.origin]
-        #check for 180-turns
-        if backlog.length > 1 and all((edge.destination == prevprevnode.data for prevprevnode in backlog.root.children)):
+    def backtrack(node: Coords, depth: int, current_index = -1) -> List[Coords]:
+        #return list of predecessors of given level of recursion
+        prevs = backlog[current_index][node]
+        if depth + current_index == 0:
+            return prevs
+        else:
+            return [xy for elem in map(lambda n: backtrack(n, depth, current_index - 1), prevs) for xy in elem]
+
+    @cache
+    def is_straight(a: Coords, b: Coords, max_straight = max_straight) -> bool:
+        return ((abs(a.x - b.x) == max_straight + 1) or (abs(a.y - b.y) == max_straight + 1))
+
+    def is_valid(edge: Edge, max_straight = max_straight) -> bool:
+        #check for 180-turn
+        if len(backlog) < 1:
+            return True
+        test = backtrack(edge.origin, 1)
+        if all(x == edge.destination for x in backtrack(edge.origin, 1)):
             return False
-        #check for straight routes
-        if backlog.length >= max_straight:
-            if all((abs(edge.destination.x - lastnode.data.x) == max_straight + 1) or (abs(edge.destination.y - lastnode.data.y) == max_straight + 1) for lastnode in backlog.leafs):
+        #check if straight
+        if len(backlog) == max_straight:
+            if all(is_straight(edge.destination, last) for last in backtrack(edge.origin, max_straight)):
                 return False
         return True
     
     for i in range(1,len(nodes)):
         print(i)
         current = defaultdict(lambda: INF)
-        newbacklog: dict[Coords, Tree[Coords]] = {}
-        for node, inputs in node_inputs.items():
-            valid_inputs = (edge for edge in inputs if not (previous[edge.origin] == INF) and is_valid(edge, backlog))
-            new_dist_by_input = sorted(((previous[edge.origin] + edge.distance, edge) for edge in valid_inputs), key = lambda x: x[0])
-            try:
-                min_dist, min_edges = next(groupby(new_dist_by_input, key = lambda x: x[0]))
-            except StopIteration:
+        previous_nodes: dict[Coords, List[Coords]] = {}
+        for edge in edges:
+            curr_distance = current[edge.destination]
+            new_distance = previous[edge.origin] + edge.distance
+            if new_distance > curr_distance:
                 continue
-            current[node] = min_dist
-            shortest[node] = min(shortest[node], min_dist)
-            tree = Tree(node)
-            for dist, edge in min_edges:
-                tree.append(backlog[edge.origin])
-            newbacklog[node] = tree
-        backlog = newbacklog
+            if new_distance < curr_distance:
+                if not is_valid(edge):
+                    continue
+                current[edge.destination] = new_distance
+                previous_nodes[edge.destination] = [edge.origin]
+                if new_distance < shortest[edge.destination]:
+                    shortest[edge.destination] = new_distance
+            if new_distance != INF and new_distance == curr_distance:
+                if is_valid(edge):
+                    previous_nodes[edge.destination].append(edge.origin)
+        backlog.append(previous_nodes)
         previous = current
 
     return shortest[finish]
