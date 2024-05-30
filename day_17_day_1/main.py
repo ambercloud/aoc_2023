d116163c11f53e32894d28e778ffafa6d4693ff7
ambercloud <dummy@example.com>
2024-05-30 23:07:06 +0300

    wjatever

--- day_17_day_1/main.py
+++ day_17_day_1/main.py
@@ -2,43 +2,14 @@ from __future__ import annotations
 from typing import List, NamedTuple, Iterator
 from dataclasses import dataclass, field
 from enum import Enum
-import heapq
-import itertools
-
-class PQueue:
-    REMOVED = '<removed-task>'      # placeholder for a removed task
-
-    def __init__(self) -> None:
-        self.pq = []                         # list of entries arranged in a heap
-        self.entry_finder = {}               # mapping of tasks to entries
-        self.counter = itertools.count()     # unique sequence count
-
-    def add_task(self, task, priority=0):
-        'Add a new task or update the priority of an existing task'
-        if task in self.entry_finder:
-            self.remove_task(task)
-        count = next(self.counter)
-        entry = [priority, count, task]
-        self.entry_finder[task] = entry
-        heapq.heappush(self.pq, entry)
-     
-    def remove_task(self, task):
-        'Mark an existing task as REMOVED.  Raise KeyError if not found.'
-        entry = self.entry_finder.pop(task)
-        entry[-1] = PQueue.REMOVED
-
-    def pop_task(self):
-        'Remove and return the lowest priority task. Raise KeyError if empty.'
-        while self.pq:
-            priority, count, task = heapq.heappop(self.pq)
-            if task is not PQueue.REMOVED:
-                del self.entry_finder[task]
-                return task
-        raise KeyError('pop from an empty priority queue')
+from collections import defaultdict
 
 class Coords(NamedTuple):
     x: int
     y: int
+
+    def __repr__(self) -> str:
+        return f'({self.x}, {self.y})'
 
 class Direction(Enum):
     n = 1
@@ -46,17 +17,47 @@ class Direction(Enum):
     s = 3
     w = 4
 
+class Edge(NamedTuple):
+    origin: Coords
+    destination: Coords
+    distance: int
+    direction: Direction
+
+    def __repr__(self) -> str:
+        return f'Edge:{self.origin}, {self.destination}, {self.distance}, {self.direction}'
+
 @dataclass
 class Node:
     xy: Coords
-    min_distance_from: dict[Coords: int] = field(default_factory=dict)
-    #edges: node, distance, direction
-    outs: list[tuple[Node, int, Direction]] = field(default_factory=list) #outward edge: node, distance, direction
-    ins: list[tuple[Node, int, Direction]] = field(default_factory=list) #inward edge: node, distance, direction
+    min_distance_from: dict[Coords, int] = field(default_factory=dict)
+
+class EdgeCollection:
+    def __init__(self) -> None:
+        self._orig_dest: dict[tuple[Coords,Coords], Edge] = {}
+        self._orig: defaultdict[Coords, List[Edge]] = defaultdict(list)
+        self._dest: defaultdict[Coords, List[Edge]] = defaultdict(list)
+
+    def add(self, edge: Edge) -> None:
+        self._orig_dest[(edge.origin, edge.destination)] = edge
+        self._orig[edge.origin].append(edge)
+        self._dest[edge.destination].append(edge)
+
+    def get_by_orig_dest(self, orig_dest: tuple[Coords, Coords]) -> Edge:
+        return self._orig_dest[orig_dest]
     
-    #stop debugger from stucking on circular refs
-    def __repr__(self) -> str:
-        return (f'Node({self.xy})')
+    def get_by_orig(self, orig: Coords) -> List[Edge]:
+        return self._orig[orig]
+    
+    def get_by_dest(self, dest: Coords) -> List[Edge]:
+        return self._dest[dest]
+    
+    def delete(self, orig_dest: tuple[Coords, Coords]) -> None:
+        if orig_dest in self._orig_dest:
+            del self._orig_dest[orig_dest]
+
+class Graph(NamedTuple):
+    nodes: dict[Coords, Node]
+    edges: EdgeCollection
 
 def gen_neighbours(xo: int, yo: int, xrange: range, yrange: range) -> Iterator[tuple[int, int, Direction]]:
     dir = Direction
@@ -65,57 +66,49 @@ def gen_neighbours(xo: int, yo: int, xrange: range, yrange: range) -> Iterator[t
         if x in xrange and y in yrange:
             yield x, y, d
 
-def parse_input(filename) -> List[List[Node]]:
+def parse_input(filename: str) -> List[List[Node]]:
     f = open(filename, 'r', encoding='utf-8')
     costs = []
     for y, line in enumerate(f):
         costs.append([int(x) for x in line.removesuffix('\n')])
+    return costs
 
+def build_graph(costs: List[List[int]]) -> Graph:
+    #returns a tuple (nodes, edges)
     nodes: dict[Coords, Node] = {}
     for y, row in enumerate(costs):
-        for x, cost in enumerate(row):
-            node = Node((x, y))
-            nodes[(x, y)] = node
+        for x, origin_cost in enumerate(row):
+            origin = Node((x, y))
+            nodes[(x, y)] = origin
 
     xmax = len(costs[0])
     ymax = len(costs)
+
+    edges = EdgeCollection()
     
     for y, row in enumerate(costs):
-        for x, cost in enumerate(row):
-            node = nodes[(x, y)]
-            for xn, yn, d in gen_neighbours(x, y, range(0, xmax), range(0, ymax)):
-                neighbour = nodes[(xn, yn)]
-                neighbour_cost = costs[yn][xn]
-                #create edges: outward edge in current node to every neighbour and inward edge from current node to every neighbour
-                node.outs.append((neighbour, neighbour_cost, d))
-                neighbour.ins.append((node, cost, d))
+        for x, origin_cost in enumerate(row):
+            origin = nodes[(x, y)]
+            for xn, yn, direction in gen_neighbours(x, y, range(0, xmax), range(0, ymax)):
+                destination = nodes[(xn, yn)]
+                destination_cost = costs[yn][xn]
+                #fill edges
+                origin_to_destination = Edge(Coords(x,y), Coords(xn, yn), destination_cost, direction)
+                edges.add(origin_to_destination)
+    
+    return Graph(nodes, edges)
 
-    return nodes
+def find_path(graph: Graph, start: Coords, end: Coords, max_straight: int) -> List[Edge]:
+    nodes, edges = graph
+    for i in range(len(nodes)):
+        for edge in edges:
+            if
+    
 
-def mark_min_cost_to_reach(nodes: dict[Coords, Node], origin: Node) -> None:
-    #marks min distance from origin to every other node, modified Dijkstra algorithm
-    nodes_to_check = PQueue()
-    visited: set[Coords] = set()
-    o_xy = origin.xy
-    origin.min_distance_from[o_xy] = 0
-    current = origin
-    while current:
-        visited.add(current.xy)
-        unchecked = [(neighbour, distance, direction) for neighbour, distance, direction in current.outs if not neighbour.xy in visited]
-        for neighbour, distance, direction in unchecked:
-            if not o_xy in neighbour.min_distance_from or (current.min_distance_from[o_xy] + distance) < neighbour.min_distance_from[o_xy]:
-                neighbour.min_distance_from[o_xy] = current.min_distance_from[o_xy] + distance
-            try:
-                nodes_to_check.remove_task(neighbour.xy)
-            except KeyError:
-                pass
-            nodes_to_check.add_task(neighbour.xy, neighbour.min_distance_from[o_xy])
-        try:
-            next_xy = nodes_to_check.pop_task()
-            current = nodes[next_xy]
-        except KeyError:
-            break
-
-nodes = parse_input('input.txt')
-mark_min_cost_to_reach(nodes, nodes[(0,0)])
+costs = parse_input('input.txt')
+graph = build_graph(costs)
+start = (0,0)
+finish = (len(costs[0]) - 1, len(costs) - 1)
+max_straight = 4
+shortest = find_path(graph, start, finish, max_straight)
 pass
